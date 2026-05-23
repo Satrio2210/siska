302e0521f55584758d6b83ea5cb874bb127481b9
Satrio2210 <satriobayu0110@gmail.com>
2026-05-23 13:19:36 +0700

    revisi interval2

--- TRXAPATI07V.php
+++ TRXAPATI07V.php
@@ -75,7 +75,7 @@ if ($panjangkata == 0 )
               FROM trxaregi
               WHERE TRXA_REGI_STAT = 'W' 
               AND TRXA_VIEW_STAT = 'Y' 
-              AND DATE(TRXA_ENTR_DATE) >= CURDATE() - INTERVAL 2 DAYS 
+              AND DATE(TRXA_ENTR_DATE) >= CURDATE() - INTERVAL 2 DAY
               ORDER BY TRXA_ENTR_DATE DESC, TRXA_ENTR_TIME DESC"; 
 }
 else
@@ -91,7 +91,7 @@ else
               FROM trxaregi
               WHERE TRXA_REGI_STAT = 'W'
               AND TRXA_VIEW_STAT = 'Y' 
-              AND DATE(TRXA_ENTR_DATE) >= CURDATE() - INTERVAL 2 DAYS
+              AND DATE(TRXA_ENTR_DATE) >= CURDATE() - INTERVAL 2 DAY
               AND (
                   TRXA_PATI_CODE LIKE '%$kata%' 
                   OR (SELECT PATI_MAIN_NAME FROM patimast WHERE PATI_MAST_CODE=TRXA_PATI_CODE) LIKE '%$kata%' 
