f7ac484fcff265e94a5a3012a96c6c4bd9e72d53
Satrio2210 <satriobayu0110@gmail.com>
2026-06-18 14:41:51 +0700

    input signa tanpa akses

--- TRXAPOLI05.php
+++ TRXAPOLI05.php
@@ -4,138 +4,136 @@
 session_start();
 
 //cek adanya session
-if (ISSET($_SESSION['username']))
-{
-	$user = $_SESSION['username'];
+if (isset($_SESSION['username'])) {
+  $user = $_SESSION['username'];
 
-?>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Sistem Informasi Klinik Pratama">
-<title>Data Signa</title>
-<link rel="shortcut icon" href="img/icon.png">
-<link rel="stylesheet" href="css/pure/pure-min.css">
-<!--[if lte IE 8]>
-	<link rel="stylesheet" href="css/layouts/side-menu-old-ie.css">
+  ?>
+  <html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Sistem Informasi Klinik Pratama">
+    <title>Data Signa</title>
+    <link rel="shortcut icon" href="img/icon.png">
+    <link rel="stylesheet" href="css/pure/pure-min.css">
+    <!--[if lte IE 8]>
+  <link rel="stylesheet" href="css/layouts/side-menu-old-ie.css">
 <![endif]-->
-<!--[if gt IE 8]><!-->
-	<link rel="stylesheet" href="css/layouts/side-menu.css">
-<!--<![endif]-->
-<style type="text/css">
-    img {
-      width: 100px;
-      height: 100px;
-      position: relative;
-      top: 0px;
-      left: 0px;
+    <!--[if gt IE 8]><!-->
+    <link rel="stylesheet" href="css/layouts/side-menu.css">
+    <!--<![endif]-->
+    <style type="text/css">
+      img {
+        width: 100px;
+        height: 100px;
+        position: relative;
+        top: 0px;
+        left: 0px;
       }
 
-	.button-view {
-    	background: rgb(28, 184, 65);
-            /* this is an green */
-        }
+      .button-view {
+        background: rgb(28, 184, 65);
+        /* this is an green */
+      }
 
-	.button-delete {
-            background: rgb(202, 60, 60);
-            /* this is an maroon */
-        }
+      .button-delete {
+        background: rgb(202, 60, 60);
+        /* this is an maroon */
+      }
+    </style>
 
-</style>
+    <style type="text/css">
+      div.footerdate {
+        position: fixed;
+        left: 50;
+        bottom: 50px;
+        width: 90%;
+        color: black;
+        text-align: right;
+      }
 
-<style type="text/css">
-	div.footerdate {
-   position: fixed;
-   left: 50;
-   bottom: 50px;
-   width: 90%;
-   color: black;
-   text-align: right;
-}
-div.footertime {
-   position: fixed;
-   left: 50;
-   bottom: 20px;
-   width: 90%;
-   color: black;
-   text-align: right;
-}
-</style>
-</head>
-<script type="text/javascript" src="js/jquery.js"></script>
-<script src="js/sanie.js"></script>
-<script src="js/sweetalert.min.js"></script>
+      div.footertime {
+        position: fixed;
+        left: 50;
+        bottom: 20px;
+        width: 90%;
+        color: black;
+        text-align: right;
+      }
+    </style>
+  </head>
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script src="js/sanie.js"></script>
+  <script src="js/sweetalert.min.js"></script>
 
-<script>
-$(document).ready(function() 
-{
-    setInterval(timestamp, 1000);
-});  
-    function timestamp() { $.ajax({ url: 'inc/timestamp.php', success: function(data) { $('#timestamp').html(data); }, }); }
-</script>
+  <script>
+    $(document).ready(function () {
+      setInterval(timestamp, 1000);
+    });
+    function timestamp() { $.ajax({ url: 'inc/timestamp.php', success: function (data) { $('#timestamp').html(data); }, }); }
+  </script>
 
-<body onLoad="periksaakses('PASS_TRXA_POLI');
-">
-<div id="layout">
-	<!-- Menu toggle -->
-	<a href="#menu" id="menuLink" class="menu-link">
+  <!-- <body onLoad="periksaakses('PASS_TRXA_POLI');
+"> -->
+
+  <body onload="ambilscreen('');">
+    <div id="layout">
+      <!-- Menu toggle -->
+      <a href="#menu" id="menuLink" class="menu-link">
         <!-- Hamburger icon -->
         <span></span>
-    	</a>
-	<!-- Menu Kiri -->
-    	<div id="menu">
-        	<div class="pure-menu">
+      </a>
+      <!-- Menu Kiri -->
+      <div id="menu">
+        <div class="pure-menu">
 
-            		<a class="pure-menu-heading" href="#"><?php echo $_SESSION['username'];?></a>
-				<ul class="pure-menu-list">
+          <a class="pure-menu-heading" href="#"><?php echo $_SESSION['username']; ?></a>
+          <ul class="pure-menu-list">
 
-              <li class="pure-menu-item" onclick="javascript: location.href = 'index.php'">
-                <a class="pure-menu-link">RAWAT JALAN</a>
-              </li>
+            <li class="pure-menu-item" onclick="javascript: location.href = 'index.php'">
+              <a class="pure-menu-link">RAWAT JALAN</a>
+            </li>
 
-              <li class="pure-menu-item" onclick="javascript: location.href = 'TRXAPOLI01.php'">
-                <a class="pure-menu-link">Pasien</a>
-              </li>
+            <li class="pure-menu-item" onclick="javascript: location.href = 'TRXAPOLI01.php'">
+              <a class="pure-menu-link">Pasien</a>
+            </li>
 
-              <li class="pure-menu-item menu-item-divided pure-menu-selected">
-                <a class="pure-menu-link">Signa</a>
-              </li>
+            <li class="pure-menu-item menu-item-divided pure-menu-selected">
+              <a class="pure-menu-link">Signa</a>
+            </li>
 
-              <li class="pure-menu-item" onclick="javascript: location.href = 'TRXAMEDI01.php'">
-                <a class="pure-menu-link">Tindakan Medis</a>
-              </li>
+            <li class="pure-menu-item" onclick="javascript: location.href = 'TRXAMEDI01.php'">
+              <a class="pure-menu-link">Tindakan Medis</a>
+            </li>
 
-              <li class="pure-menu-item" onclick="javascript: location.href = 'DIAGMAST01.php'">
-                <a class="pure-menu-link">Kode Diagnosa</a>
-              </li>
+            <li class="pure-menu-item" onclick="javascript: location.href = 'DIAGMAST01.php'">
+              <a class="pure-menu-link">Kode Diagnosa</a>
+            </li>
 
-              <li class="pure-menu-item" onclick="javascript: location.href = 'signout.php'">
-                <a class="pure-menu-link">EXIT</a>
-              </li>
+            <li class="pure-menu-item" onclick="javascript: location.href = 'signout.php'">
+              <a class="pure-menu-link">EXIT</a>
+            </li>
 
-				</ul>
-		</div>
-    	</div><!-- div menu -->
-	
-	<!-- tampilan menu -->
-	<div id="main">
+          </ul>
+        </div>
+      </div><!-- div menu -->
+
+      <!-- tampilan menu -->
+      <div id="main">
         <div class="header">
-            <img align="right" 
-                 height= "<?php echo $width_logo;?>" 
-                 width= "<?php echo $height_logo;?>" 
-                 src="img/logo.png" 
-                 alt="">
+          <img align="right" height="<?php echo $width_logo; ?>" width="<?php echo $height_logo; ?>" src="img/logo.png"
+            alt="">
 
-            <h1 id="login">Sistem Informasi Klinik Pratama</h1>
-            <h2>SISKA</h2>
+          <h1 id="login">Sistem Informasi Klinik Pratama</h1>
+          <h2>SISKA</h2>
         </div><!-- div header -->
         <div class="headerlogo">
         </div>
 
-		<div class="content">
+        <div class="content">
 
-        <!-- Tab Menu -->
+          <!-- Tab Menu -->
           <div class="pure-menu pure-menu-horizontal">
             <ul class="pure-menu-list">
 
@@ -147,21 +145,16 @@ $(document).ready(function()
 
             </ul>
           </div>
-    <!-- Tab Menu -->
+          <!-- Tab Menu -->
 
-    <!-- Form Input -->
-    <form name="frmtrxapoli" class="pure-form pure-form-aligned" method="post" action="">
-    	<fieldset>
+          <!-- Form Input -->
+          <form name="frmtrxapoli" class="pure-form pure-form-aligned" method="post" action="">
+            <fieldset>
 
-      		<div class="pure-control-group">
+              <div class="pure-control-group">
 
-        		<label for="txtsgnacode">Kode Signa :</label>
-              	<input type="text" 
-            	  	name="txtsgnacode" 
-              		id="txtsgnacode" 
-              		maxlength ="2"
-              		style="width: 50px;"
-                  onkeyup="var start = this.selectionStart;
+                <label for="txtsgnacode">Kode Signa :</label>
+                <input type="text" name="txtsgnacode" id="txtsgnacode" maxlength="2" style="width: 50px;" onkeyup="var start = this.selectionStart;
                           var end = this.selectionEnd;
                           this.value = this.value.toUpperCase();
                           this.setSelectionRange(start, end);
@@ -174,19 +167,14 @@ $(document).ready(function()
                             document.getElementById('txtsgnaname').setAttribute('disabled','true');
                           }  
                           "
-                onkeydown="if (event.keyCode == 13 && value.length > 2) document.getElementById('txtsgnaname').focus()">
+                  onkeydown="if (event.keyCode == 13 && value.length > 2) document.getElementById('txtsgnaname').focus()">
 
-          </div><!-- pure-control-group --> 
+              </div><!-- pure-control-group -->
 
-          <div class="pure-control-group">
+              <div class="pure-control-group">
 
-            <label for="txtsgnaname">Keterangan Signa :</label>
-              <input type="text" 
-                  name="txtsgnaname" 
-                  id="txtsgnaname" 
-                  maxlength ="50"
-                  style="width: 300px;"
-              onkeydown="if (event.keyCode == 13 && value.length > 0)
+                <label for="txtsgnaname">Nama Signa :</label>
+                <input type="text" name="txtsgnaname" id="txtsgnaname" maxlength="50" style="width: 300px;" onkeydown="if (event.keyCode == 13 && value.length > 0)
               {
                   if (document.getElementById('txtsgnacode').value == '')
                   {
@@ -216,14 +204,16 @@ $(document).ready(function()
                        input(insgnacode,insgnaname);
                     }
 
-              }"
-              >
+              }">
 
-          </div><!-- pure-control-group -->
-
-      </fieldset>
-      <fieldset>
-          <a class="pure-button pure-button-primary" onclick="javascript:
+              </div><!-- pure-control-group -->
+              <div class="pure-control-group">
+                <label for="txtsgnausag">Cara Pemakaian :</label>
+                <input type="text" name="txtsgnausag" id="txtsgnausag" maxlength="50" style="width: 300px;" readonly>
+              </div>
+            </fieldset>
+            <fieldset>
+              <a class="pure-button pure-button-primary" onclick="javascript:
                   if (document.getElementById('txtsgnacode').value == '')
                   {
                     swal({
@@ -253,52 +243,46 @@ $(document).ready(function()
                     }
 
         ">Submit</a>
-        </fieldset>
+            </fieldset>
 
-        <fieldset>
-        <label for="txtsearch">Cari...</label>
-        <input type="text" class="pure-input-rounded"
-            name="txtsearch" 
-            id="txtsearch" 
-            maxlength ="20"
-            style="width: 200px;"
-            onkeyup="if (value.length > 0) { ambilscreen(this.value); } else {ambilscreen('')};"
-
-            onkeydown="if (event.keyCode == 13 && value.length > 0) 
+            <fieldset>
+              <label for="txtsearch">Cari...</label>
+              <input type="text" class="pure-input-rounded" name="txtsearch" id="txtsearch" maxlength="20"
+                style="width: 200px;" onkeyup="if (value.length > 0) { ambilscreen(this.value); } else {ambilscreen('')};"
+                onkeydown="if (event.keyCode == 13 && value.length > 0) 
                         { 
                         document.getElementById('txtsearch').value = '';
                         document.getElementById('txtsearch').focus()
                         }">
-        </fieldset>
+            </fieldset>
 
 
-      <fieldset>
-          <div id="tblscreen">
-      </fieldset>
+            <fieldset>
+              <div id="tblscreen">
+            </fieldset>
 
-    </form>
+          </form>
 
-		</div><!-- div content -->
-<div class="footerdate">
-  	<span class="labelTime Time"><b>Date  :</b> <?php $tgl=date('d-m-Y'); echo $tgl;?></span>
-</div>
-<div class="footertime">
-	<span class = "labelTime Time" id="timestamp"></span>
-</div>
+        </div><!-- div content -->
+        <div class="footerdate">
+          <span class="labelTime Time"><b>Date :</b> <?php $tgl = date('d-m-Y');
+          echo $tgl; ?></span>
+        </div>
+        <div class="footertime">
+          <span class="labelTime Time" id="timestamp"></span>
+        </div>
 
 
-    	</div><!-- div main -->
-</div><!-- div layout -->
-<script src="js/TRXAPOLI05.js"></script>
-<script src="js/ui.js"></script>
+      </div><!-- div main -->
+    </div><!-- div layout -->
+    <script src="js/TRXAPOLI05.js"></script>
+    <script src="js/ui.js"></script>
 
-</body>
-</html>
-<?php
-}
-else
+  </body>
 
-{
-  header("Location: "."signin.php");
+  </html>
+  <?php
+} else {
+  header("Location: " . "signin.php");
 }
 ?>
--- js/TRXAPOLI05.js
+++ js/TRXAPOLI05.js
@@ -1,56 +1,56 @@
   // periksa akses
-    var aksesku;
-  function periksaakses(fieldid)
-  {
-    aksesku = buatajaxakses();
-    var url="TRXAPOLI01X-AKSES.php";
-    aksesku.onreadystatechange=stateChangedAkses;
-    var params = "q="+fieldid;
-    //alert('Parameter adalah '+fieldid);
-    aksesku.open("POST",url,true);
-    aksesku.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    aksesku.setRequestHeader("Content-length", params.length);
-    aksesku.setRequestHeader("Connection", "close");
-    aksesku.send(params);
-
-  }
-
-  function buatajaxakses()
-  {
-    if (window.XMLHttpRequest)
-    {
-    return new XMLHttpRequest();
-    }
-    if (window.ActiveXObject)
-    {
-    return new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    return null;
-  }
-
-  function stateChangedAkses()
-  {
-  var data;
-  if (aksesku.readyState==4)
-    {
-
-    xdata=aksesku.responseText;
-    data=xdata.trim();
+  //   var aksesku;
+  // function periksaakses(fieldid)
+  // {
+  //   aksesku = buatajaxakses();
+  //   var url="TRXAPOLI01X-AKSES.php";
+  //   aksesku.onreadystatechange=stateChangedAkses;
+  //   var params = "q="+fieldid;
     
-    if(data.length>1)
-      {
-        document.getElementById('txtsgnacode').focus();
-        document.getElementById('txtsgnaname').setAttribute('disabled','true');
-        ambilscreen('');      
-      }
-      else
-      {
-        document.getElementById('txtsgnacode').setAttribute('disabled','true');
-        document.getElementById('txtsgnaname').setAttribute('disabled','true');
-        document.getElementById('txtsearch').setAttribute('disabled','true');
-      }
-    }
-  }
+  //   aksesku.open("POST",url,true);
+  //   aksesku.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  //   aksesku.setRequestHeader("Content-length", params.length);
+  //   aksesku.setRequestHeader("Connection", "close");
+  //   aksesku.send(params);
+
+  // }
+
+  // function buatajaxakses()
+  // {
+  //   if (window.XMLHttpRequest)
+  //   {
+  //   return new XMLHttpRequest();
+  //   }
+  //   if (window.ActiveXObject)
+  //   {
+  //   return new ActiveXObject("Microsoft.XMLHTTP");
+  //   }
+  //   return null;
+  // }
+
+  // function stateChangedAkses()
+  // {
+  // var data;
+  // if (aksesku.readyState==4)
+  //   {
+
+  //   xdata=aksesku.responseText;
+  //   data=xdata.trim();
+    
+  //   if(data.length>1)
+  //     {
+  //       document.getElementById('txtsgnacode').focus();
+  //       document.getElementById('txtsgnaname').setAttribute('disabled','true');
+  //       ambilscreen('');      
+  //     }
+  //     else
+  //     {
+  //       document.getElementById('txtsgnacode').setAttribute('disabled','true');
+  //       document.getElementById('txtsgnaname').setAttribute('disabled','true');
+  //       document.getElementById('txtsearch').setAttribute('disabled','true');
+  //     }
+  //   }
+  // }
 // end periksa akses  
 
   // periksa Kode signa
